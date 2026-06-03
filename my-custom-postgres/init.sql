2c23d47f41a78485973320ef2dacc1a135a6c39a
CodeCrafterX9 <prachiofficial14@gmail.com>
2026-06-03 22:47:13 +0000

    test postgres connection

--- my-custom-postgres/init.sql
+++ my-custom-postgres/init.sql
@@ -6,3 +6,12 @@ CREATE TABLE   ny_taxi_data (
     fare_amount FLOAT,
     payment_status VARCHAR(50)
 );
+
+CREATE TABLE  table2 (
+    id INT,
+    customer_name VARCHAR(100),
+    signup_date DATE,
+    trip_distance FLOAT,
+    fare_amount FLOAT,
+    payment_status VARCHAR(50)
+);
