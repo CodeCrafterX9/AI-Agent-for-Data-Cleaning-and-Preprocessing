0ed32a82d3d9aef3f488fbac05b9e88ef8837eda
CodeCrafterX9 <prachiofficial14@gmail.com>
2026-06-03 08:48:13 +0000

    run postgres connection

---
+++ my-custom-postgres/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS sample_taxi_data (
+    id INT,
+    customer_name VARCHAR(100),
+    signup_date DATE,
+    trip_distance FLOAT,
+    fare_amount FLOAT,
+    payment_status VARCHAR(50)
+);
