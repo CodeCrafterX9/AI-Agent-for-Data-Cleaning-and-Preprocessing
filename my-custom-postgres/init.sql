f056f51f0476ff75878b46ba546f44596a9cfd7b
CodeCrafterX9 <prachiofficial14@gmail.com>
2026-06-03 23:05:12 +0000

    confirm connection with github codespace

--- my-custom-postgres/init.sql
+++ my-custom-postgres/init.sql
@@ -15,3 +15,12 @@ CREATE TABLE  table2 (
     fare_amount FLOAT,
     payment_status VARCHAR(50)
 );
+
+CREATE TABLE  table3 (
+    id INT,
+    customer_name VARCHAR(100),
+    signup_date DATE,
+    trip_distance FLOAT,
+    fare_amount FLOAT,
+    payment_status VARCHAR(50)
+);
